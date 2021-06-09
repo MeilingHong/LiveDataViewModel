e1da9bda3ef5a6529d74717c147fa1634834c630
huangzhou <marisareimu@126.com>
2021-06-09 08:59:49 +0800

    Gson 工具，含读取Assets文件夹中的json文件；密码框组件；

--- component_utils/src/main/java/com/meiling/component/utils/gson/GsonFormatUtils.java
+++ component_utils/src/main/java/com/meiling/component/utils/gson/GsonFormatUtils.java
@@ -12,39 +12,51 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GsonFormatUtils {
-
-	public static <T> T geObjectFromJSON(String str, Type type) {
-		if (!TextUtils.isEmpty(str)) {
-			return Gsons.getInstance().fromJson(str, type);
-		}
-		return null;
-	}
-
-	public static <T> Type getType(T t){
-		return new TypeToken<T>(){}.getType();
-	}
-
-	public static <T> Type getListType(T t){
-		return new TypeToken<List<T>>(){}.getType();
-	}
-
-//	public static <T> T getObjFromJSON(String str, Class<T> cls) {
-//		if (!TextUtils.isEmpty(str)) {
-//			return Gsons.getInstance().fromJson(str, cls);
-//		}
-//		return null;
-//	}
+    /*
+     使用List类型进行转换后，直接使用时可能会抛出该异常
+      【java.lang.ClassCastException: com.google.gson.internal.LinkedTreeMap cannot be cast to】
+     */
+//    @Deprecated
+//    public static <T> T geObjectFromJSON(String str, Type type) {
+//        if (!TextUtils.isEmpty(str)) {
+//            return Gsons.getInstance().fromJson(str, type);
+//        }
+//        return null;
+//    }
 //
-//	public static <T> List<T> getObjectList(String jsonString,Class<T> cls){
-//		List<T> list = new ArrayList<T>();
-//		try {
-//			JsonArray arry = new JsonParser().parse(jsonString).getAsJsonArray();
-//			for (JsonElement jsonElement : arry) {
-//				list.add(Gsons.getInstance().fromJson(jsonElement, cls));
-//			}
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
-//		return list;
-//	}
+//    public static <T> Type getType(T t) {
+//        return new TypeToken<T>() {
+//        }.getType();
+//    }
+//
+//    public static <T> Type getListType(T t) {
+//        return new TypeToken<List<T>>() {
+//        }.getType();
+//    }
+
+    /**
+     * 将String转换成单个实体对象
+     */
+    public static <T> T getObjectJson(String str, Class<T> cls) {
+        if (!TextUtils.isEmpty(str)) {
+            return Gsons.getInstance().fromJson(str, cls);
+        }
+        return null;
+    }
+
+	/**
+	 * 将String转换成列表实体对象
+	 */
+    public static <T> List<T> getObjectList(String jsonString, Class<T> cls) {
+        List<T> list = new ArrayList<T>();
+        try {
+            JsonArray arry = new JsonParser().parse(jsonString).getAsJsonArray();
+            for (JsonElement jsonElement : arry) {
+                list.add(Gsons.getInstance().fromJson(jsonElement, cls));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
 }
